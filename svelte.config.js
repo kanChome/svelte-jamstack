4d3aabf5a27d2e769617fc972d6d5ef3954771d1
kanChome <study.hellbird@gmail.com>
2023-07-16 11:28:41 +0900

    ssg setting.

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,27 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		scss: {
+			sourceMap: true,
+			includePaths: ['src'],
+			renderSync: true
+		}
+	}),
 
+	// SSGするための設定
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'buikd',
+			assets: 'build',
+			fallback: null,
+			preprocess: false,
+			strict: true
+
+		})
 	}
 };
 
