06c41ba3081cdd60e5fe0c122210631337d82aa3
kanChome <study.hellbird@gmail.com>
2023-07-17 12:21:01 +0900

    add deply setting.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const isDev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -24,8 +26,10 @@ const config = {
 			fallback: null,
 			preprocess: false,
 			strict: true
-
-		})
+		}),
+		paths: {
+			base: isDev ? '' : process.env.BASE_PATH,
+		}
 	}
 };
 
